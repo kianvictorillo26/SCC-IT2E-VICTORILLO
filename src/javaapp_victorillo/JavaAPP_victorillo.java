8c769e9435f0852375631021b9198bdf4e6d91e3
kianvictorillo26 <kianvictorillo@gmail.com>
2024-09-04 17:09:50 +0800

    new update

--- src/javaapp_victorillo/JavaAPP_victorillo.java
+++ src/javaapp_victorillo/JavaAPP_victorillo.java
@@ -10,28 +10,31 @@ public class JavaAPP_victorillo {
         Sales sales = new Sales();
         Products pr = new Products();
         
-        int choice;
-        
-        do{
-            System.out.println("1.Grades\n2.Product\n3.Sales\n");
-            System.out.print("Enter Choice: ");
-            choice = sc.nextInt();
-            
-        switch (choice){
-            
-            case 1:
-                 gr.getGrades();
-                 break;
-            case 2:
-                 pr.getProducts();
-                break;
-            case 3:
-                sales.getSales();
-                break;
-            case 4:
-                break;
-          }
-        }while( choice > 3 || choice < 0 );
+//        int choice;
+//        
+//        do{
+//            System.out.println("1.Grades\n2.Product\n3.Sales\n4.Accounts");
+//            System.out.print("Enter Choice: ");
+//            choice = sc.nextInt();
+//            
+//        switch (choice){
+//            
+//            case 1:
+//                 gr.getGrades();
+//                 break;
+//            case 2:
+//                 pr.getProducts();
+//                break;
+//            case 3:
+//                sales.getSales();
+//                break;
+//            case 4:
+//                acr.getAccounts();
+//                break;
+//            case 5:
+//                break;
+//          }
+//        }while( choice > 4 || choice < 0 );
 //        Grades gr1 = new Grades();
 //        gr1.addGrades(1011,"jhon mark ",3.3,3.2,3.3,3.3);
 //        gr1.viewGrades();
@@ -46,6 +49,11 @@ public class JavaAPP_victorillo {
 
 //          Sales sales = new Sales();
 //          sales.getSales();
+
+           Account acr = new Account();
+           acr.getAccounts();
+
+
         
     }
     
